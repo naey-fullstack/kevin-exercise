6e77bd8ab31498a33cf7884378afb1ed2264dad9
Naey <naey@github.com>
2019-12-16 17:21:08 +0800

    finished layout

--- src/scripts/modules/theme-module.js
+++ src/scripts/modules/theme-module.js
@@ -12,6 +12,16 @@ AppName.Modules.ThemeModule = (function() {
       pagination: {
         el: '.swiper-pagination',
       },
+      speed: 600,
+      parallax: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
     });
   };
 
